a5df99bdafdf0055d82d0c46cb665ed02ac989a6
nnhatsang <nhatsang57@gmail.com>
2024-01-24 00:40:53 +0700

    update cardlist

--- src/Services/configServ.js
+++ src/Services/configServ.js
@@ -5,7 +5,7 @@ const token = dataUser?.token;
 export const API = axios.create({
   baseURL: "https://airbnbnew.cybersoft.edu.vn",
   headers: {
-    TokenCybersoft: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAwOSIsIkhldEhhblN0cmluZyI6IjE4LzA1LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxNTk5MDQwMDAwMCIsIm5iZiI6MTY5MjI5MTYwMCwiZXhwIjoxNzE2MTM4MDAwfQ.qCglC_oyHM79HVc5mRXJfocVkww4VUpWO7ug7MWtJoY`,
+    TokenCybersoft: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAwMSIsIkhldEhhblN0cmluZyI6IjMwLzA5LzIwMzEiLCJIZXRIYW5UaW1lIjoiMTk0ODQ5MjgwMDAwMCIsIm5iZiI6MTYwMTIyNjAwMCwiZXhwIjoxOTQ4NjQwNDAwfQ.4l-eTzlgVnFczfvc2Or7BNPOcaesY3Kwc8RoNm-o-6M`,
     Authorization: `Bearer ${dataUser ? dataUser.accessToken : null}`,
     token,
   },
